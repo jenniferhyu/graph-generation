dd28a12fbe92ada74ea84b627ece266cee6d5379
jenniferhyu <jyu127@berkeley.edu>
2015-04-30 17:07:45 -0700

    Increase ability of random graph generation

--- graph_generation.py
+++ graph_generation.py
@@ -10,11 +10,13 @@ import random
 	\     /       /
 	 4           6
 	  \         /
-	  	    5 
-The diamond's vertices are numbered like so"""
+	  	   5 
+The diamond's vertices are numbered like so
+
+This is adapated from Papadimitriou, Steiglitz 1978"""
 
 
-def diamond_fill(adj_matrix):
+def diamond_fill(adj_matrix, east_west):
 	"""Fills adjacency matrix with edge weights proposed in paper
 	Vertex numbering follows clockwise movement. Goes in cycles of 0 to 7 (mod 8)"""
 	counter = 0
@@ -23,28 +25,43 @@ def diamond_fill(adj_matrix):
 			for j in range(0,8):
 				x = counter*diamond + i #counter gives us which ``kth'' diamond we're at
 				y = counter*diamond + j
-				if (x == y):
-					adj_matrix[x,y] = 0
-					adj_matrix[y,x] = 0
-				elif (i == west and j == sw_mid) or (i == sw_mid and j == west):
-					adj_matrix[x,y] = 0
-				elif (i == east and j == ne_mid) or (i == ne_mid and j == east):
-					adj_matrix[x,y] = 0
-				else:
-					adj_matrix[x,y] = 1
+				if east_west:
+					if (x == y):
+						adj_matrix[x,y] = 0
+						adj_matrix[y,x] = 0
+					elif (i == west and j == sw_mid) or (i == sw_mid and j == west):
+						adj_matrix[x,y] = 0
+					elif (i == east and j == ne_mid) or (i == ne_mid and j == east):
+						adj_matrix[x,y] = 0
+					else:
+						adj_matrix[x,y] = 1
+				else: 
+					if (x == y):
+						adj_matrix[x,y] = 0
+						adj_matrix[y,x] = 0
+					elif (i == north and j == nw_mid) or (i == nw_mid and j == north):
+						adj_matrix[x,y] = 0
+					elif (i == south and j == se_mid) or (i == se_mid and j == south):
+						adj_matrix[x,y] = 0
+					else:
+						adj_matrix[x,y] = 1
 		counter+=1
 	#print(adj_matrix.item((0,0)))
 	#print(adj_matrix)
 
-def east_west(adj_matrix):
-	"""Fills in the adj_matrix for the east-west connection between diamonds
+def connect_diamond(adj_matrix, east_west):
+	"""Fills in the adj_matrix for the connection between diamonds
 	Temporarily hard-coded version"""
 	for i in range(0, vertices):
 		for j in range(0, vertices):
 			if (abs(i-j)) == 1:
-				if ((i%diamond == 7 or i%diamond == 0) and (j%diamond == 7 or j%diamond == 0)):
-					#print((i,j))
-					adj_matrix[i,j] = 1
+				if east_west:
+					if ((i%diamond == 7 or i%diamond == 0) and (j%diamond == 7 or j%diamond == 0)):
+						#print((i,j))
+						adj_matrix[i,j] = 1
+				else:
+					if ((i%diamond == 2 or i%diamond == 5) and (j%diamond == 2 or j%diamond == 5)):
+						adj_matrix[i,j] = 1
 	# adj_matrix[vertices-1, 0] = 1
 	# adj_matrix[0, vertices-1] = 1
 	#temporarily commenting the block out because we might not need to return to our start point 
@@ -91,21 +108,26 @@ if __name__ == '__main__':
 	diamond = 8 #8 vertices in a diamond
 	k = vertices//diamond #how many diamond circuits we'll have
 
+	east_west = True
+
 	north = 2
 	south = 5
 	west = 0
 	east = 7
 	sw_mid = 4
 	ne_mid = 3
+	nw_mid = 1
+	se_mid = 6
 
 	max_edge_val = 100
+	#max_edge_val = random.randint(90,100,2) #generate random even integer as 2M
 
 	adj_matrix = np.empty(shape=[vertices, vertices])
-	adj_matrix.fill(0)
+	adj_matrix.fill(max_edge_val)
 
-	diamond_fill(adj_matrix)
+	diamond_fill(adj_matrix,east_west)
 	isolate(adj_matrix)
-	east_west(adj_matrix)
+	connect_diamond(adj_matrix,east_west)
 	colors = random_color_assignment(vertices)
 	adj_matrix = adj_matrix.astype(int)
 	with open("1.in", "w") as f:
